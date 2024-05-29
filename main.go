10428aa246e678e0634d8467a13077f73ccef98f
cybercoder <vahid.lid@live.com>
2024-05-29 19:11:44 +0330

    Initial Add event

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	k8s "github.com/ik8s-ir/beaver/pkg/k8s"
+	"github.com/ik8s-ir/beaver/pkg/ovsnet"
+	"k8s.io/client-go/tools/cache"
 )
 
 func main() {
 
-	ovsInformer := k8s.RunOVSInformer()
+	ovsInformer := k8s.CreateOVSInformer()
+	ovsInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: ovsnet.AddEvent,
+	})
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 	go ovsInformer.Run(stopCh)
--- pkg/ovsnet/ovsnet.go
+++ pkg/ovsnet/ovsnet.go
@@ -1,9 +1,14 @@
 package ovsnet
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/ik8s-ir/beaver/pkg/helpers"
+	"github.com/ik8s-ir/beaver/pkg/k8s"
 	"github.com/ik8s-ir/beaver/pkg/types"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -14,11 +19,47 @@ var bridge string = ""
 
 func AddEvent(obj interface{}) {
 	unstructuredObj := obj.(*unstructured.Unstructured)
-	ovsnet := &types.OvsNet{}
+	on := &types.OvsNet{}
 
-	converter.FromUnstructured(unstructuredObj.Object, ovsnet)
+	converter.FromUnstructured(unstructuredObj.Object, on)
 
-	bridge = helpers.NextBridgeID(bridge, 4)
-	log.Println(ovsnet.Namespace, ovsnet.Name)
+	bridge = helpers.NextBridgeID(bridge, 12)
+	log.Println(on.Namespace, on.Name)
+	CreateDestributedVswitch(bridge)
 	// ovsagent.CreateOVSNetwork(bridge, []string{"172.16.16.1"})
+
+	// 1. retrieve all kubernetes compute nodes.
+}
+
+func CreateDestributedVswitch(bridge string) {
+	// 1. fertch compute nodes
+	nodes := k8s.FetchComputeNodes()
+
+	for _, node := range nodes.Items {
+		pod := k8s.GetOVSPodByNode("kube-system", node.GetName())
+		done := false
+		retry := 0
+		for !done {
+			_, err := createVswitch(bridge, "http://"+pod.GetName()+".kube-system.svc.cluster.local")
+			if err == nil {
+				done = true
+			}
+			log.Printf("Failed to create vswitch on pod %s , node %s.\n %v \n", pod.GetName(), node.GetName(), err)
+
+			retry++
+			if retry > 10 {
+				log.Fatalf("Hasn't success after 10 times.")
+			}
+			log.Printf("Retry %v/10 in 2 seconds ...\n", retry)
+			time.Sleep(2 * time.Second)
+		}
+	}
+}
+
+func createVswitch(bridge string, url string) (resp *http.Response, err error) {
+	body := &types.VswitchPostBody{
+		Bridge: bridge,
+	}
+	jsonBody, _ := json.Marshal(body)
+	return http.Post(url, "application/json", bytes.NewBuffer(jsonBody))
 }
