4482dc47c7a2bd6c4e8f8d011c0e3f368f2528c3
cybercoder <vahid.lid@live.com>
2024-05-30 13:17:41 +0330

    enable dotenv

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
+
 	k8s "github.com/ik8s-ir/beaver/pkg/k8s"
 	"github.com/ik8s-ir/beaver/pkg/ovsnet"
+	"github.com/joho/godotenv"
 	"k8s.io/client-go/tools/cache"
 )
 
 func main() {
-
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("The .env file not loaded")
+	}
 	ovsInformer := k8s.CreateOVSInformer()
 	ovsInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: ovsnet.AddEvent,
