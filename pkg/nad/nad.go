97cfd517e5be98f7b0e1513750ace48673f05ef2
cybercoder <vahid.lid@live.com>
2024-06-01 00:44:04 +0330

    create ovsnet on ovs type NAD.

--- pkg/nad/nad.go
+++ pkg/nad/nad.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/ik8s-ir/beaver/pkg/helpers"
 	"github.com/ik8s-ir/beaver/pkg/k8s"
 	"github.com/ik8s-ir/beaver/pkg/types"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -15,16 +16,41 @@ var converter = runtime.DefaultUnstructuredConverter
 func AddEvent(obj interface{}) {
 	unstructuredObj := obj.(*unstructured.Unstructured)
 	nad := &types.NetworkAttachmentDefinition{}
-
 	converter.FromUnstructured(unstructuredObj.Object, nad)
 
-	log.Println(nad.GetNamespace(), nad.GetName())
-	nadConfig := &types.NADConfig{}
-	json.Unmarshal([]byte(nad.Spec.Config), nadConfig)
-	log.Println(nadConfig.Type)
-	if nad.GetName() == "default" || nadConfig.Type != "ovs" {
+	var nadConfig map[string]interface{}
+	err := json.Unmarshal([]byte(nad.Spec.Config), &nadConfig)
+	if err != nil {
+		log.Println("Error unmarshaling nad config json: %v", err)
 		return
 	}
-	k8s.CreateOVSnet(nad.GetName(), nad.GetNamespace())
-	// k8s.DeleteOVSnet(nad.GetName(), nad.GetNamespace())
+
+	if nad.GetName() == "default" || nadConfig["type"] != "ovs" {
+		return
+	}
+
+	for {
+		bridgeName := helpers.CreateUniqueTimeName()
+		_, err := k8s.CreateOVSnet(bridgeName, nad.GetNamespace(), nad.GetName())
+		if err != nil {
+			log.Printf("error on creating %s (cluster level) for namespace %s, err: %v \n retrying...\n", nad.GetName(), nad.GetNamespace(), err)
+		} else {
+			if nadConfig["bridge"] != "" {
+				break
+			}
+			nadConfig["bridge"] = bridgeName
+			jsonNadConfig, _ := json.Marshal(nadConfig)
+			nad.Spec.Config = string(jsonNadConfig)
+			nadMap, _ := converter.ToUnstructured(nad)
+			for {
+				_, err := k8s.UpdateNAD(&unstructured.Unstructured{Object: nadMap})
+				if err == nil {
+					break
+				}
+				log.Printf("Error updating NAD %s at the namespace %s with ovs bridge named: %s, error: %v \nretrying... \n", nad.GetName(), nad.GetNamespace(), bridgeName, err)
+			}
+			break
+		}
+	}
+	// k8s.DeleteOVSnet(nad.GetName())
 }
